939436498fde447db1af958caa40fd7da71c10b0
KacperM45440 <dsw45440@student.dsw.edu.pl>
2022-11-19 12:56:42 +0100

    Wstepny commit
    Inicjalizacja projektu w unity

---
+++ Assets/Scripts/testscript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
