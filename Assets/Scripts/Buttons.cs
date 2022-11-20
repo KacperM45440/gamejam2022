8f73b4a97ef3abca124d38a1d48bf0dd64cfe9b8
Zyrafa <mikisuper0@gmail.com>
2022-11-20 10:30:16 +0100

    missing sources

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,28 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    [SerializeField] private string clickPlay = "Play";
-    //[SerializeField] private string clickQuit = "Quit";
-    [SerializeField] private string clickAutors = "Autorzy";
-    [SerializeField] private string clickBack = "Menu";
+    //[SerializeField] private string clickPlay = "Play";
+    ////[SerializeField] private string clickQuit = "Quit";
+    //[SerializeField] private string clickAutors = "Autorzy";
+    //[SerializeField] private string clickBack = "Menu";
 
-    public void NewBackButton()
-    {
-        SceneManager.LoadScene(clickBack);
+    //public void NewBackButton()
+    //{
+    //    SceneManager.LoadScene(clickBack);
 
-    }
-    public void NewPlayButton()
-    {
-        SceneManager.LoadScene(clickPlay);
+    //}
+    //public void NewPlayButton()
+    //{
+    //    SceneManager.LoadScene(clickPlay);
 
-    }
-    public void NewAutorsButton()
-    {
-        SceneManager.LoadScene(clickAutors);
+    //}
+    //public void NewAutorsButton()
+    //{
+    //    SceneManager.LoadScene(clickAutors);
 
-    }
-    public void NewQuitButton()
-    {
-        Application.Quit();
-    }
+    //}
+    //public void NewQuitButton()
+    //{
+    //    Application.Quit();
+    //}
 }
